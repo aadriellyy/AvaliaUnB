e43f915211c9fc19c6fd5710fd2cff08203b5de7
aadriellyy <limaadrielly214@gmail.com>
2024-07-29 21:35:11 -0300

    adicionando classes

---
+++ src/classes/classes/Aluno.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class Aluno extends Pessoa{
+
+ private int matricula;
+ private String curso;
+ private String senha;
+ 
+ public Aluno(String nome, String departamento, String email, int matricula, String curso, String senha){
+    
+ }
+
+}
---
+++ src/classes/classes/Pessoa.java
@@ -0,0 +1,34 @@
+package classes;
+
+
+public class Pessoa {
+
+    /*VARIAVEIS: nome, departamento e email*/
+    private String name;
+    private String departamento;
+    private String email;
+    static int id;
+
+    /*Construtor da classe*/
+    public Pessoa(String name, String departamento, String email){
+        this.name = name;
+        this.departamento = departamento;
+        this.email = email;
+        id++;
+    }
+
+    public Pessoa(){}
+
+    public String getNome(){
+        return this.name;
+    }
+
+    public String getDepartamento(){
+        return this.departamento;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+    
+}
